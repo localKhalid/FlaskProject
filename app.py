de2aa777c1bdfb4b3365ff839aa84c2ee05c6e5b
Khalid Abdulle <118912865+localKhalid@users.noreply.github.com>
2023-04-26 11:11:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,25 @@ def handle_information():
     # Map priority values to queue names
     priority_mapping = {
         'high_priority': 'High',
-        'medium_priority
+        'medium_priority': 'MediumLow',
+        'low_priority': 'MediumLow'
+    }
+
+    # Get the queue name based on priority
+    queue_name = priority_mapping.get(priority)
+
+    if queue_name is not None:
+        # Send the JSON data to the appropriate SQS queue
+        sqs.send_message(
+            QueueUrl=f'https://sqs.{os.environ.get("AWS_REGION", "eu-north-1")}.amazonaws.com/301073929141/{queue_name}',
+            MessageBody=json.dumps(bug_form)
+        )
+
+        # SEND INFORMATION TO ANOTHER API?????
+        return jsonify({'message': 'Your bug form has been accepted. Thank you'}), 200
+    else:
+        return jsonify({'error': 'Invalid priority value'}), 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
