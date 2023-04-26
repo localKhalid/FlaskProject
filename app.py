a2c284658fb9b12f1376bf16b7f413302981b2c3
Khalid Abdulle <118912865+localKhalid@users.noreply.github.com>
2023-04-26 12:10:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app = Flask(__name__, template_folder='.')
 # Create an SQS client
 sqs = boto3.client('sqs', region_name='eu-north-1')
 
+
 @app.route("/")
 def home():
     return render_template('upload_form.html')
+
 
 @app.route('/handle_information', methods=['POST'])
 def handle_information():
@@ -37,21 +39,24 @@ def handle_information():
     if queue_name:
         try:
             # Construct the queue URL
-            qURl = sqs.get_queue_url(QueueName=priority)['QueueUrl']
+            qURl = sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
             # Send message to the queue with a 10-second delay
             response = sqs.send_message(
-                QueueUrl= qURl,
+                QueueUrl=qURl,
                 DelaySeconds=10,
                 MessageBody=json.dumps(bug_form)
             )
 
-            return jsonify({'message': 'Bug information submitted successfully', 'MessageId': response['MessageId']}), 200
+            return jsonify(
+                {'message': 'Bug information submitted successfully', 'MessageId': response['MessageId']}), 200
 
         except Exception as e:
             return jsonify({'error': 'Failed to submit bug information to the queue', 'details': str(e)}), 500
 
     else:
-        return jsonify({'error': 'Invalid priority value', 'details': 'Priority value selected in the form is not valid'}), 400
+        return jsonify(
+            {'error': 'Invalid priority value', 'details': 'Priority value selected in the form is not valid'}), 400
+
 
 @app.route('/createQueues', methods=['GET'])
 def create_queues():
@@ -63,6 +68,7 @@ def create_queues():
     sqs.create_queue(QueueName='DLQ')
 
     return jsonify({'message': 'Queues created successfully'}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
