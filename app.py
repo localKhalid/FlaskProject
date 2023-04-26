f1466af657cda18be3bc49c4685b06e1ee3a6646
Khalid Abdulle <118912865+localKhalid@users.noreply.github.com>
2023-04-26 11:41:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('upload_form.html')
 
 @app.route('/handle_information', methods=['POST'])
 def handle_information():
